fbe2b5817087ade8bb4ac09fd85e9000c3a017b7
Anna Prokopeva <46624560+heyanny2@users.noreply.github.com>
2022-01-24 16:31:00 +0300

    Update 3-export element by index.js

--- 2-arrays/3-export element by index.js
+++ 2-arrays/3-export element by index.js
@@ -16,25 +16,25 @@ export default getValueFromArray;
 const cities = ['moscow', 'london', 'berlin', 'porto', '', null, undefined];
 
 const actual1 = getValueFromArray(cities, 0);
-console.log(actual1);//moscow
+console.log(actual1);//'moscow'
 
 const actual2 = getValueFromArray(cities, 2, 'default');
-console.log(actual2);//berlin
+console.log(actual2);//'berlin'
 
 const actual3 = getValueFromArray(cities, 7, false);
-console.log(actual3);//false
+console.log(actual3);//'false'
 
 const actual4 = getValueFromArray(cities, -1, 'oops');
-console.log(actual4);//oops
+console.log(actual4);//'oops'
 
 const actual5 = getValueFromArray(cities, 7);
-console.log(actual5);//null
+console.log(actual5);//'null'
 
 const actual6 = getValueFromArray(cities, 4);
-console.log(actual6);//
+console.log(actual6);//''
 
 const actual7 = getValueFromArray(cities, 5, 'default');
-console.log(actual7);// null
+console.log(actual7);// 'null'
 
 const actual8 = getValueFromArray(cities, 6, 'default');
-console.log(actual8);//undefined
+console.log(actual8);//'undefined'
