af69549d4880a65ccafb05519051f2b712c71b1b
Anna Prokopeva <46624560+heyanny2@users.noreply.github.com>
2022-08-11 16:54:04 +0300

    Create 3-comptrss images.js

---
+++ 11-Tree structure/exersices/3-comptrss images.js
@@ -0,0 +1,32 @@
+/* Реализуйте и экспортируйте функцию compressImages(), которая принимает на вход директорию, находит внутри нее картинки и "сжимает" их. 
+Под сжиманием понимается уменьшение свойства size в метаданных в два раза. 
+Функция должна вернуть новую директорию со сжатыми картинками и всеми остальными данными, которые были внутри этой директории.
+
+Картинками считаются все файлы заканчивающиеся на .jpg.
+
+const tree = mkdir('my documents', [
+  mkfile('avatar.jpg', { size: 100 }),
+  mkfile('passport.jpg', { size: 200 }),
+  mkfile('family.jpg', { size: 150 }),
+  mkfile('addresses', { size: 125 }),
+  mkdir('presentations')
+]);
+ 
+const newTree = compressImages(tree);*/
+
+
+export const compressImages = (tree) => {
+  const children = getChildren(tree);
+  const newChildren = children.map((child) => {
+    const name = getName(child);
+    if (!isFile(child) || !name.endsWith('.jpg')) {
+      return child;
+    }
+    const meta = getMeta(child);
+    const newMeta = _.cloneDeep(meta);
+    newMeta.size /= 2;
+    return mkfile(name, newMeta);
+  });
+  const newMeta = _.cloneDeep(getMeta(tree));
+  return mkdir(getName(tree), newChildren, newMeta);
+};
