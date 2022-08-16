dd3259eb635fb51888234b4c493bfda87fc2871c
Anna Prokopeva <46624560+heyanny2@users.noreply.github.com>
2022-08-16 17:33:52 +0300

    Update 5-get hidden files count.js

--- 11-Tree structure/exersices/5-get hidden files count.js
+++ 11-Tree structure/exersices/5-get hidden files count.js
@@ -1,26 +1,5 @@
 /*Реализуйте и экспортируйте по умолчанию функцию, которая считает количество скрытых файлов в директории и всех поддиректориях. 
-Скрытым файлом в Linux системах считается файл, название которого начинается с точки. 
-
-import { mkdir, mkfile } from '@hexlet/immutable-fs-trees';
-import getHiddenFilesCount from '../getHiddenFilesCount.js';
- 
-const tree = mkdir('/', [
-  mkdir('etc', [
-    mkdir('apache'),
-    mkdir('nginx', [
-      mkfile('.nginx.conf', { size: 800 }),
-    ]),
-    mkdir('.consul', [
-      mkfile('.config.json', { size: 1200 }),
-      mkfile('data', { size: 8200 }),
-      mkfile('raft', { size: 80 }),
-    ]),
-  ]),
-  mkfile('.hosts', { size: 3500 }),
-  mkfile('resolve', { size: 1000 }),
-]);
- 
-getHiddenFilesCount(tree); // 3 */
+Скрытым файлом в Linux системах считается файл, название которого начинается с точки.*/
 
 const getHiddenFilesCount = (tree) => {
   const name = getName(tree);
@@ -33,3 +12,40 @@ const getHiddenFilesCount = (tree) => {
 };
 
 export default getHiddenFilesCount;
+
+//tests
+
+const tree1 = mkdir('/', [
+    mkdir('etc', [
+      mkdir('apache'),
+      mkdir('nginx', [
+        mkfile('.nginx.conf', { size: 800 }),
+      ]),
+      mkdir('.consul', [
+        mkfile('.config.json', { size: 1200 }),
+        mkfile('data', { size: 8200 }),
+        mkfile('raft', { size: 80 }),
+      ]),
+    ]),
+    mkfile('.hosts', { size: 3500 }),
+    mkfile('resolve', { size: 1000 }),
+  ]);
+
+console.log(getHiddenFilesCount(tree1));//3
+
+const tree2 = mkdir('/', [
+    mkdir('.etc', [
+      mkdir('.apache'),
+      mkdir('nginx', [
+        mkfile('.nginx.conf', { size: 800 }),
+      ]),
+    ]),
+    mkdir('.consul', [
+      mkfile('config.json', { size: 1200 }),
+      mkfile('.raft', { size: 80 }),
+    ]),
+    mkfile('hosts', { size: 3500 }),
+    mkfile('resolve', { size: 1000 }),
+  ]);
+
+console.log(getHiddenFilesCount(tree1));//2
