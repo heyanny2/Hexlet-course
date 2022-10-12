d5f198334956d7fa1828629e4e553a107b62e36f
Anna Prokopeva <46624560+heyanny2@users.noreply.github.com>
2022-10-12 11:37:50 +0300

    Create 2-this.js

---
+++ 12-OOP/2-this.js
@@ -0,0 +1,35 @@
+/* Реализуйте и экспортируйте по умолчанию функцию, которая создает рациональное число. Рациональное число должно быть представлено объектом со следующими методами:
+
+Сеттер setNumer() - устанавливает числитель
+Сеттер setDenom() - устанавливает знаменатель
+Геттер getNumer() - возвращает числитель
+Геттер getDenom() - возвращает знаменатель
+Геттер toString() - возвращает строковое представление числа
+Метод add() - складывает дробь на которой он был вызван с переданной дробью и возвращает новое рациональное число (не изменяет текущее!) */
+
+const make = (numer, denom) => ({
+  numer,
+  denom,
+  setNumer(newNumer) {
+    this.numer = newNumer;
+  },
+  setDenom(newDenom) {
+    this.denom = newDenom;
+  },
+  getNumer() {
+    return this.numer;
+  },
+  getDenom() {
+    return this.denom;
+  },
+  toString() {
+    return `${this.getNumer()}/${this.getDenom()}`;
+  },
+  add(rational) {
+    const newNumer = (this.getNumer() * rational.getDenom() + this.getDenom() * rational.getNumer());
+    const newDenom = this.getDenom() * rational.getDenom();
+    return make(newNumer, newDenom);
+  },
+});
+
+export default make;
