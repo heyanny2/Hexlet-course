8cecaaa6034a8538d4e6d34f0c4ebf3225810af1
Anna Prokopeva <46624560+heyanny2@users.noreply.github.com>
2023-02-08 13:11:22 +0300

    Create 4-normalize(fluent interface).js

---
+++ 13-Object-oriented design/4-normalize(fluent interface).js
@@ -0,0 +1,32 @@
+/*Реализуйте и экспортируйте по умолчанию функцию normalize() которая принимает на вход список городов и стран, нормализует их имена, сортирует города и группирует их по стране.
+
+import normalize from './solution.js';
+ 
+const countries = [
+  { name: 'Miami', country: 'usa' },
+  { name: 'samarA', country: '  ruSsiA' },
+  { name: 'Moscow ', country: ' Russia' },
+];
+ 
+normalize(countries);
+// {
+//   russia: [
+//     'moscow',
+//     'samara',
+//   ],
+//   usa: [
+//     'miami',
+//   ],
+// } */
+
+export default (countries) => countries
+  .map(({ name, country }) => ({ city: name.toLowerCase(), country: country.toLowerCase() }))
+  .map(({ city, country }) => ({ city: city.trim(), country: country.trim() }))
+  .map(({ city, country }) => [country, city])
+  .sort()
+  .reduce((acc, [country, city]) => {
+    const citiesAcc = acc[country] ?? [];
+    const cities = citiesAcc.concat(city);
+    const uniqueCities = new Set(cities);
+    return { ...acc, [country]: [...uniqueCities] };
+  }, {});
