37c10ef0539f4e196ad142caed9738a458c43d01
Anna Prokopeva <46624560+heyanny2@users.noreply.github.com>
2023-03-27 22:41:11 +0300

    Create 6-normalize classes.js

---
+++ 15-JS: DOM API/6-normalize classes.js
@@ -0,0 +1,21 @@
+/*Реализуйте и экспортируйте по умолчанию функцию, которая нормализует имена классов для всех элементов на странице. В данном случае это означает, что происходит преобразование всех классов, написанных с использованием kebab нотации, в camelCase нотацию: text-center => textCenter
+
+Попробуйте решить эту задачу без использования регулярных выражений.
+
+Примеры
+// <body>
+//   <div class="text-center row-b">Bam</div>
+// </body>
+normalize(document);
+console.log(document.body.innerHTML);
+// <body>
+//   <div class="textCenter rowB">Bam</div>
+// </body> */
+
+export default (document) => {
+  const allNodes = [...document.body.getElementsByTagName('*')];
+  allNodes.forEach((el) => {
+    const classes = el.classList;
+    classes.forEach((curClass) => classes.replace(curClass, camelCase(curClass)));
+  });
+};
