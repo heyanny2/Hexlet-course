e4bd9fba6a4a5527f5d96500ae6324ef32b94345
Anna Prokopeva <46624560+heyanny2@users.noreply.github.com>
2023-04-07 21:08:04 +0200

    Create 13-carousel.js

---
+++ 15-JS: DOM API/13-carousel.js
@@ -0,0 +1,32 @@
+/* Реализуйте логику слайдера в функции экспортированной по умолчанию.
+
+Постройте свою логику так, чтобы она позволила использовать на одной странице любое количество компонентов carousel с любым количеством картинок внутри.
+
+Решите задачу, используя методы jQuery.*/
+
+
+import $ from 'jquery';
+
+export default () => {
+  const buttons = $('a[data-slide]');
+  $(buttons).click((e) => {
+    const slideDirection = e.currentTarget.dataset.slide;
+    const carousel = e.currentTarget.closest('[data-ride="carousel"]');
+    const active = $('.carousel-item.active', carousel);
+    const firstSibling = $(active).siblings().first();
+    const lastSibling = $(active).siblings().last()
+    if (slideDirection === 'next') {
+      if ($(active).next().length === 0) {
+        $(active).removeClass('active');
+        $(firstSibling).addClass('active');
+      }
+      $(active).removeClass('active').next().addClass('active');
+    } else {
+      if ($(active).prev().length === 0) {
+        $(active).removeClass('active');
+        $(lastSibling).addClass('active');
+      }
+      $(active).removeClass('active').prev().addClass('active');
+    }
+  });
+};
