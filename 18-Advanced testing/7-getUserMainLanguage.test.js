5cf0b8dc8d6b3632569e949ba7ed64b82a7abb06
Anna Prokopeva <46624560+heyanny2@users.noreply.github.com>
2023-05-08 18:41:20 +0200

    Create 7-getUserMainLanguage.test.js

---
+++ 18-Advanced testing/7-getUserMainLanguage.test.js
@@ -0,0 +1,46 @@
+/*Протестируйте функцию getUserMainLanguage(username, client), 
+которая определяет язык на котором пользователь создал больше всего репозиториев. 
+Для реализации этой задачи, функция getUserMainLanguage() выполняет запрос через @octokit/rest, 
+который извлекает все репозитории указанного пользователя (по первому параметру username). 
+Каждый репозиторий в этом списке содержит указание основного языка репозитория. 
+Эта информация используется для поиска того языка, который используется чаще. 
+Если список репозиториев пуст, функция возвращает null.
+
+Пример:
+// Запрос который выполняет функция getUserMainLanguage
+// Именно этот метод нужно будет подменить в фейковом клиенте
+const { data } = await client.repos.listForUser({ username });
+// data – список репозиториев. У каждого репозитория может быть много полей
+// но нас интересует ровно одно – language
+// Эти данные нужно подготовить в тестах для фейкового клиента
+console.log(data);
+// [{ language: 'php', ... }, { language: 'javascript', ... }, ...]*/
+
+class OctokitFake {
+  constructor(data) {
+    this.data = data;
+  }
+
+  repos = {
+    listForUser: () => (
+      Promise.resolve({ data: this.data })
+    ),
+  };
+}
+
+test('get user mail language', async () => {
+  const data = [
+    { language: 'php' },
+    { language: 'javascript' },
+    { language: 'CSS' },
+  ];
+  const client = new OctokitFake(data);
+  const mainLanguage = await getUserMainLanguage('user', client);
+  expect(mainLanguage).toEqual('php');
+});
+
+test('getUserMainLanguage when empty', async () => {
+  const client = new OctokitFake([]);
+  const mainLanguage = await getUserMainLanguage('user-without-repos', client);
+  expect(mainLanguage).toBeNull();
+});
