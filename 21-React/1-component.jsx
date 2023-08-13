b986e58e5610e9b0e8222c82d31a84b8f7d2d962
Anna Prokopeva <46624560+heyanny2@users.noreply.github.com>
2023-08-13 22:55:04 +0200

    Update 1-component.jsx

--- 21-React/1-component.jsx
+++ 21-React/1-component.jsx
@@ -1,12 +1,7 @@
-/*Реализуйте компонент Card, возвращающий следующий jsx:
-
-<div className="card">
-  <div className="card-body">
-    <h4 className="card-title">Card title</h4>
-    <p className="card-text">Some quick example text to build on the card</p>
-    <button type="button" className="btn btn-primary">Go somewhere</button>
-  </div>
-</div>*/
+/*
+Write a realization for the Bootstrap's card component using React. 
+Export it to index.jsx and render it inside the DOM element with id #container.
+*/
 ///showcase on CodePen https://codepen.io/heyannny2/pen/VwVJeKa
 import ReactDOM from 'react-dom/client';
 import React from 'react';
