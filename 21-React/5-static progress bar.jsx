3834b559125c56396a244edceb615cfa2502b429
Anna Prokopeva <46624560+heyanny2@users.noreply.github.com>
2023-08-14 23:04:04 +0200

    Update and rename 5-jsx-html-difference.jsx to 5-static progress bar.jsx

--- 21-React/5-static progress bar.jsx
+++ 21-React/5-static progress bar.jsx
@@ -1,4 +1,5 @@
 /*Write a realization for a static Bootstrap's progress bar that takes current position as a prop.*/
+//showcase on codePen https://codepen.io/heyannny2/pen/abQejxa
 
 import ReactDOM from 'react-dom/client';
 import React from 'react';
