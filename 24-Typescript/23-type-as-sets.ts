903ed72fa3c389bb26bd13147ea1a97e927416fd
Anna Prokopeva <46624560+heyanny2@users.noreply.github.com>
2024-04-20 18:45:04 +0200

    Create 23-type-as-sets.ts

---
+++ 24-Typescript/23-type-as-sets.ts
@@ -0,0 +1,6 @@
+/*Определите и экспортируйте тип CustomType, который может принимать следующие значения:
+1.null
+2.undefined
+3.любое число */
+
+export type CustomType = null | undefined | number;
